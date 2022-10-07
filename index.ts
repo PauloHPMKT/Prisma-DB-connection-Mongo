5084e1f0b4b251e4a491e79dd21f2adf19c2ad84
Paulo Sergio <pauloserg861@gmail.com>
2022-10-07 19:41:47 -0300

    criando modelo para collection de user

--- index.ts
+++ index.ts
@@ -3,18 +3,41 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.$connect()
+  await prisma.$connect()
+
+  //creating a user collection in db
+  await prisma.user.create({
+    data: {
+      name: 'Paulo Sergio',
+      email: 'paulosergio@example.com',
+      posts: {
+        create: {
+          title: 'My first prisma post',
+          body: 'A very interesting stuff',
+          slug: 'My first post',
+        },
+      },
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+
+  console.dir(allUsers, { depth: null })
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-        //creating prisma queries to database
-        const allUsers = await prisma.user.findMany()
-        console.log(allUsers)
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect()
+    //creating prisma queries to database
+  })
+
+
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
